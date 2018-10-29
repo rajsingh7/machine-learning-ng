56aa0a848e2b0db682f08d777c4cd3cc28374e3c
Dan Reardon <reardon.danielj@gmail.com>
2018-10-29 17:12:39 -0400

    params complete

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,19 +15,46 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+Cs= [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigmas =[0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+for i = 1:length(Cs)
+  C=Cs(i);
 
+  for j = 1:length(sigmas)
+    sigma = sigmas(j);
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    error(j) = mean(double(predictions ~= yval));
 
+    if (j==1)
+      error_j = error(j);
+    else
+      error_j = [error_j; error(j)];
+    end
+  end
 
+  if (i==1)
+    error_i = error_j;
+  else
+    error_i = [error_i error_j];
+  end
+end
 
+% Locate the index of the minimum value in matrix error_i
+[r,c] = find(error_i == min(min(error_i)));
+
+% Optimal values
+C = Cs(c);
+sigma = sigmas(r);
 
 % =========================================================================
 
